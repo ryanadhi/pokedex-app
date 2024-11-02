80e896b5357974a0e128a3e485b0758d4888ba4c
ryanadhi <ryanadhi.p@gmail.com>
2024-11-03 01:10:14 +0700

    feat: optimize detailscreen for small screen

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -1,9 +1,9 @@
-import { GetServerSideProps } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Inter } from 'next/font/google';
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
 interface Ability {
   ability: {
@@ -28,67 +28,194 @@ interface Pokemon {
   weight: number;
   abilities: Ability[];
   types: Type[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
 }
 
 interface PokemonDetailProps {
   pokemon: Pokemon;
+  color: string;
 }
 
-export default function PokemonDetail({ pokemon }: PokemonDetailProps) {
+const isColorLight = (color: string) => {
+  const lightColors = ["white", "yellow", "pink"];
+  return lightColors.includes(color);
+};
+
+export default function PokemonDetail({ pokemon, color }: PokemonDetailProps) {
   const capitalizeName = (name: string) => {
     return name.charAt(0).toUpperCase() + name.slice(1);
   };
 
   const getTypeId = (url: string) => {
-    const id = url.split('/').filter(Boolean).pop();
-    return id ? id : '';
+    const id = url.split("/").filter(Boolean).pop();
+    return id ? id : "";
   };
 
+  const getStatName = (name: string) => {
+    switch (name) {
+      case "hp":
+        return "HP";
+      case "attack":
+        return "ATK";
+      case "defense":
+        return "DEF";
+      case "special-attack":
+        return "SP-ATK";
+      case "special-defense":
+        return "SP-DEF";
+      case "speed":
+        return "SPEED";
+      case "accuracy":
+        return "ACCURACY";
+      case "evasion":
+        return "EVASION";
+      default:
+        return name;
+    }
+  };
   const pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
 
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-center p-8 ${inter.className}`}>
-      <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full text-center">
-        <h1 className="text-3xl font-bold mb-4">{capitalizeName(pokemon.name)}</h1>
+    <main
+      className={`w-screen px-4 pt-4 flex flex-col gap-8 ${roboto.className}`}
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed top-0 left-0 w-full bg-opacity-90 bg-inherit z-30 p-4">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center">
+        <Link href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            className={`size-6 cursor-pointer ${
+          isColorLight(color) ? "text-black" : "text-white"
+            }`}
+          >
+            <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+        </Link>
+        <h1
+          className={`text-2xl font-semibold tracking-wider ${
+            isColorLight(color) ? "text-black" : "text-white"
+          }`}
+        >
+          {capitalizeName(pokemon.name)}
+        </h1>
+          </div>
+          <div>
+        <p className="text-xl text-slate-400">
+          #{pokemon.id.toString().padStart(3, "0")}
+        </p>
+          </div>
+        </div>
+      </div>
+      <div className="z-20 mt-20">
         <Image
           src={pokemonImageUrl}
           alt={pokemon.name}
-          width={96}
-          height={96}
-          className="mx-auto mb-4"
+          width={240}
+          height={240}
+          className="mx-auto"
         />
-        <div className="text-lg mb-2">
-          <p><strong>Height:</strong> {pokemon.height}</p>
-          <p><strong>Weight:</strong> {pokemon.weight}</p>
-          <p><strong>Types:</strong></p>
-          <div className="flex justify-center mb-2">
-            {pokemon.types.map((type) => {
-              const typeId = getTypeId(type.type.url);
-              const typeImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/${typeId}.png`;
-              return (
-                <Image
-                  key={type.type.name}
-                  src={typeImageUrl}
-                  alt={type.type.name}
-                  width={96}
-                  height={96}
-                  className="mx-2"
-                />
-              );
-            })}
-          </div>
-          <p><strong>Abilities:</strong></p>
-          <ul className="list-disc list-inside">
-            {pokemon.abilities.map((ability) => (
-              <li key={ability.ability.name}>{capitalizeName(ability.ability.name)}</li>
-            ))}
-          </ul>
+      </div>
+      <div className="bg-white flex-grow py-14 rounded-t-lg shadow-md -mt-14 z-10 flex flex-col gap-4 ">
+        <div className="flex justify-center mb-2">
+          {pokemon.types.map((type) => {
+            const typeId = getTypeId(type.type.url);
+            const typeImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/${typeId}.png`;
+            return (
+              <Image
+                key={type.type.name}
+                src={typeImageUrl}
+                alt={type.type.name}
+                width={96}
+                height={96}
+                className="mx-2"
+              />
+            );
+          })}
         </div>
-        <Link href="/">
-          <span className="text-blue-500 cursor-pointer underline">
-            Back to Pokémon List
-          </span>
-        </Link>
+        <div>
+          <h2
+            className="text-center font-bold text-xl mb-4"
+            style={{ color: isColorLight(color) ? "black" : color }}
+          >
+            About
+          </h2>
+          <div className="flex justify-around items-center">
+            <div className="text-center flex gap-2">
+              <Image
+                src="/assets/straighten.svg"
+                alt="Height Icon"
+                width={36}
+                height={36}
+                className="mx-auto mb-1"
+              />
+              <div>
+                <p className="text-2xl font-bold">
+                  {(pokemon.height / 10).toFixed(1)} m
+                </p>
+                <p className="text-sm text-gray-500">Height</p>
+              </div>
+            </div>
+            <div className="border-l border-gray-300 h-6"></div>
+            <div className="text-center flex gap-2">
+              <Image
+                src="/assets/weight.svg"
+                alt="Weight Icon"
+                width={36}
+                height={36}
+                className="mx-auto mb-1"
+              />
+              <div>
+                <p className="text-2xl font-bold">
+                  {(pokemon.weight / 10).toFixed(1)} kg
+                </p>
+                <p className="text-sm text-gray-500">Weight</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2
+            className="text-center font-bold text-xl mb-4"
+            style={{ color: isColorLight(color) ? "black" : color }}
+          >
+            Base Stats
+          </h2>
+          <div className="flex flex-col gap-4">
+            {pokemon.stats.map((stat) => (
+              <div
+                key={stat.stat.name}
+                className="flex justify-between px-4 items-center gap-4"
+              >
+                <p className="text-sm w-2/12 text-end">{getStatName(stat.stat.name)}</p>
+                <p className="w-1/12 font-semibold">{stat.base_stat}</p>
+                <div className="w-9/12 bg-gray-200 rounded-full h-2.5">
+                  <div
+                    className="h-2.5 rounded-full"
+                    style={{
+                      width: `${stat.base_stat}%`,
+                      backgroundColor: color,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
@@ -99,9 +226,14 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const pokemon = await res.json();
 
+  const speciesRes = await fetch(pokemon.species.url);
+  const species = await speciesRes.json();
+  const color = species.color?.name || "white";
+
   return {
     props: {
       pokemon,
+      color,
     },
   };
 };
